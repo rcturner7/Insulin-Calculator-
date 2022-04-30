7be75dc9d72e72de511e244d4ea08ae1c41e8faa
Ryan Turner <98337469+rcturner7@users.noreply.github.com>
2022-04-30 18:16:07 -0500

    Create insulinCalculator.py

---
+++ insulinCalculator.py
@@ -0,0 +1,12 @@
+# Create a variable asking the user what ratio they are on. 
+# 1 unit per a specific amount of carbs
+scale = int(input("What Carb/insulin ratio are you on? Type the number of carbs per one unit. ")
+
+# Create another variable asking user the amount of carbs they are going to eat with their meal. 
+ratio = int(input("How many carbs is your meal/snack? ")
+
+# This last variable will be the amount of carbs user will eat divided ÷ by the number of cards per 1
+# unit the user is on for the amount of Obagi
+units_of_insulin = ratio / scale
+
+print(f"You need to take {units_of_insulin} units of insulin. Enjoy your meal.")
